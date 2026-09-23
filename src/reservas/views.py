18c4f654d1d93d3d1dcc496f96d5044c3a3d9965
Miguel <migu.gatica@duocuc.cl>
2026-09-23 17:40:24 -0300

    implementa consulta de disponibilidad por fechas

--- src/reservas/views.py
+++ src/reservas/views.py
@@ -3,8 +3,24 @@ from .models import Habitacion
 
 
 def inicio(request):
-    habitaciones = Habitacion.objects.all()
+    habitaciones = Habitacion.objects.filter(estado='Disponible')
+
+    fecha_entrada = request.GET.get('fecha_entrada')
+    fecha_salida = request.GET.get('fecha_salida')
+
+    if fecha_entrada and fecha_salida:
+        habitaciones_ocupadas = Habitacion.objects.filter(
+            reservas__estado='Confirmada',
+            reservas__fecha_entrada__lt=fecha_salida,
+            reservas__fecha_salida__gt=fecha_entrada
+        )
+
+        habitaciones = habitaciones.exclude(
+            id__in=habitaciones_ocupadas
+        )
 
     return render(request, 'reservas/inicio.html', {
-        'habitaciones': habitaciones
+        'habitaciones': habitaciones,
+        'fecha_entrada': fecha_entrada,
+        'fecha_salida': fecha_salida,
     })

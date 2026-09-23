96bda5b55b1b8d8b3e5823891bb55c39cc1a257b
Miguel <migu.gatica@duocuc.cl>
2026-09-23 17:12:38 -0300

    integra listado de habitaciones con vista Django

--- src/reservas/views.py
+++ src/reservas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Habitacion
 
-# Create your views here.
+
+def inicio(request):
+    habitaciones = Habitacion.objects.all()
+
+    return render(request, 'reservas/inicio.html', {
+        'habitaciones': habitaciones
+    })
